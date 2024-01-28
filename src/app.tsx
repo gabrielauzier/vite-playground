666890fba58e8e540259e41629b54fc8fce9754b
gabrielauzier <gabrielvascoauzier@gmail.com>
2024-01-28 05:44:12 -0400

    patch: update some styles

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,15 @@
 import './common/styles/global.css'
 
-import { Text, Heading } from './common/presentation/components'
+import { Text, Heading, Button } from './common/presentation/components'
 
 function App() {
   return (
-    <>
+    <body className="dark:bg-zinc-900 min-h-screen ">
+      <Button variant="default">Hello</Button>
       <Heading as="h1">Theres no country for old man</Heading>
       <Text>Hello Main New</Text>
       <Text>Hello Main New</Text>
-    </>
+    </body>
   )
 }
 
