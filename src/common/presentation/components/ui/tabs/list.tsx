486694ee104df40beaaf28b2904af08e5529fe05
gabrielauzier <gabrielvascoauzier@gmail.com>
2024-01-29 02:33:33 -0400

    patch: change bg color

--- src/common/presentation/components/ui/tabs/list.tsx
+++ src/common/presentation/components/ui/tabs/list.tsx
@@ -10,7 +10,7 @@ export const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      'inline-flex h-10 items-center justify-center rounded-md bg-stone-100 p-1 text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400',
+      'inline-flex h-10 items-center justify-center rounded-md bg-zinc-100 p-1 text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400',
       className,
     )}
     {...props}
