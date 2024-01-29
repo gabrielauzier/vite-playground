d919ceb825914fd209b3ddba9d3ddfc588d7ebf3
gabrielauzier <gabrielvascoauzier@gmail.com>
2024-01-29 02:29:55 -0400

    refactor: subtitle

--- src/common/presentation/components/typography/subtitle.tsx
+++ src/common/presentation/components/typography/subtitle.tsx
@@ -1,13 +1,35 @@
-import { ReactNode } from 'react'
+import { cn } from '@/common/lib/shadcn-ui/utils'
+import React from 'react'
 
-interface SubtitleProps {
-  children: ReactNode
+// interface SubtitleProps exntends {
+//   children: ReactNode
+// }
+
+// export function Subtitle({ children }: SubtitleProps) {
+//   return (
+//     <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+//       {children}
+//     </h3>
+//   )
+// }
+
+export interface SubtitleProps
+  extends React.ButtonHTMLAttributes<HTMLHeadingElement> {
+  asChild?: boolean
 }
 
-export function Subtitle({ children }: SubtitleProps) {
-  return (
-    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-      {children}
-    </h3>
-  )
-}
+export const Subtitle = React.forwardRef<HTMLHeadingElement, SubtitleProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <h3
+        className={cn(
+          'scroll-m-20 text-2xl font-semibold tracking-tight',
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  },
+)
+Subtitle.displayName = 'Stack'
