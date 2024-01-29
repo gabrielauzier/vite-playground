5cacb614fba420b3379a6741f68fc600453a2e4b
gabrielauzier <gabrielvascoauzier@gmail.com>
2024-01-29 01:43:36 -0400

    patch: update root layout

--- src/common/presentation/layouts/root-layout.tsx
+++ src/common/presentation/layouts/root-layout.tsx
@@ -9,7 +9,7 @@ export function RootLayout({ children }: { children: ReactNode }) {
       <body className="dark:bg-zinc-900 min-h-[calc(100vh - 4rem)] max-w-[1280px] m-auto">
         <div className="flex">
           <Sidebar />
-          <main className="ml-4 mt-3">{children}</main>
+          <main className="pl-8 pt-6 w-full">{children}</main>
         </div>
       </body>
     </>
